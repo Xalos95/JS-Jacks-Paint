1157089b67c9ae5acfe064c5514db365c9ec3d72
Xalos95 <142462852+Xalos95@users.noreply.github.com>
2023-10-09 20:46:05 -0700

    set up price and color

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -30,38 +30,48 @@ function getProductInfo(paintColor) {
     
     switch (paintColor) {
         case 'pn1':           
-            // set variables for price and color name and invoke a function to update the price     
+            price = '$14.99';
+            colorName = 'Lime Green' 
+            updatePrice(price, color); 
             break;           
         case 'pn2':
-            // set variables for price and color name and invoke a function to update the price    
+            price = '$11.14';
+            colorName = 'Medium Brown';    
             break;            
         case 'pn3':
-            // set variables for price and color name and invoke a function to update the price  
+            price = '$22.99';
+            colorName = 'Royal Blue'; 
             break;   
         case 'pn4':
-            // set variables for price and color name and invoke a function to update the price  
+            price = '$13.42';
+            colorName = 'Solid Red'; 
             break;   
         case 'pn5':
-            // set variables for price and color name and invoke a function to update the price       
+            price = '$21.98';
+            colorName = 'Solid White';      
             break;   
         case 'pn6':
-            // set variables for price and color name and invoke a function to update the price        
+            price = '$4.99';
+            colorName = 'Solid Black';       
             break;   
         case 'pn7':
-            // set variables for price and color name and invoke a function to update the price 
+            price = '$8.22';
+            colorName = 'Solid Cyan'; 
             break;   
         case 'pn8':
-            // set variables for price and color name and invoke a function to update the price   
+            price = '$11.99';
+            colorName = 'Solid Purple'  
             break;   
         case 'pn9':
-            // set variables for price and color name and invoke a function to update the price 
+            price = '$14.99';
+            colorName = 'Solid Yellow';  
             break;   
           default:              
     }
 
     function updatePrice(colorName, price)
     {       
-        let colorPrice = // select element with corresponding id
+        let colorPrice = 
         // display price
         
         let color = // select element with corresponding id
